7fe49e8b70b23abde5f51c2a33763b44f167d87c
Michael Chapa <michaelchapa1@gmail.com>
2020-04-16 18:47:29 -0500

    Completed randomizer, on to the InsertionSort alg, then Dinner :9

--- assign4.c
+++ assign4.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+double *pUnsortedM, *pSortedM;
 
 int main(int argc, char* argv[]){
     if(argc != 2){
@@ -8,8 +11,23 @@ int main(int argc, char* argv[]){
     }
 
     int iArraySize = atoi(argv[1]);
+    pUnsortedM = malloc(sizeof(double) * iArraySize);
+    pSortedM = malloc(sizeof(double) * iArraySize);
+    
+    generateRandom(iArraySize);
 
-    printf("%d\n", iArraySize);
+    free(pUnsortedM);
+    free(pSortedM);
 
     return 0;
+}
+
+// Populates pUnsortedM with (n) random elements
+void generateRandom(int iArraySize){
+    srand(time(0)); // Use current time to seed random()
+    int i;
+
+    for(i = 0; i < iArraySize; i++){
+        pUnsortedM[i] = (double) (rand() % 1000) + 1;
+    }
 }
